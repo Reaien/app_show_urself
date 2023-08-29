375d760844545c59385fb5f98fc76e877989aad2
Reaien <ivanduarteherrera@gmail.com>
2023-08-29 15:58:52 -0400

    iconos de generos y seccion generos listocaaa

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -12,6 +12,7 @@ export class HomePage implements OnInit {
   swiperModules = [IonicSlides];
 
   slides: any[] = [];
+  genreSlides: any[] = [];
   data: any;
 
 
@@ -35,6 +36,17 @@ export class HomePage implements OnInit {
       {banner: '../../../assets/img/artista4.jpg'},
       {banner: '../../../assets/img/artista5.jpg'},
       {banner: '../../../assets/img/artista6.png'},
+    ]
+
+    this.genreSlides = [
+      {genre: '../../../assets/img/icon-genero1.jpg'},
+      {genre: '../../../assets/img/icon-genero2.jpg'},
+      {genre: '../../../assets/img/icon-genero3.jpg'},
+      {genre: '../../../assets/img/icon-genero4.jpg'},
+      {genre: '../../../assets/img/icon-genero5.jpg'},
+      {genre: '../../../assets/img/icon-genero6.jpg'},
+      {genre: '../../../assets/img/icon-genero7.jpg'},
+      {genre: '../../../assets/img/icon-genero8.jpg'},
     ]
 
   }
